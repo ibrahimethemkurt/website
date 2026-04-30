b0b241025c909bfbe39a8f0a890e66f38c2d8f62
ibrahimethemkurt <ibrahimethemkurt8110@gmail.oom>
2026-05-01 02:53:49 +0300

    Update contact info and make favicon white for dark mode visibility

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
           <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
         </svg>
       ),
-      url: 'https://www.instagram.com/ibrahimethemkrt/',
+      url: 'https://www.instagram.com/ibrahimethemkurtt/',
     },
     {
       name: 'E-posta',
@@ -33,7 +33,7 @@ const Contact = () => {
           <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
         </svg>
       ),
-      url: 'mailto:iletisim@ibrahimethemkurt.com',
+      url: 'mailto:ibrahimethemkurt8110@gmail.com',
     }
   ];
 
